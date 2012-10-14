f8ad411960fe285d797cca971e222e6534d6e6f0
timtim002hk <timtim002hk@gmail.com>
2012-10-14 10:26:02 -0700

    Add random IEnumerable for List, Dictionary, Collection, Array.

--- Ranbrary/IEnumerable/IEnumerableExtension.cs
+++ Ranbrary/IEnumerable/IEnumerableExtension.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
+using System.Collections.ObjectModel;
 
 namespace Ranbrary.IEnumerable
 {
-	public static class IEnumerableExtension
-	{
-		public static T RandomTakeOne<T>(this IList<T> enumerable)
-		{
+    public static class IEnumerableExtension
+    {
+        public static T RandomTakeOne<T>(this IList<T> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException("enumerable");
 
-			T token = enumerable[6];
-			return token;
-			//return Rand.TakeOne(range);
-		}
-	}
+            return enumerable[new Random().Next(1, enumerable.Count)]; ;
+        }
+
+        public static T RandomTakeOne<K, T>(IDictionary<K, T> dictionary)
+        {
+            if (dictionary == null) throw new ArgumentNullException("dictionary");
+
+            return dictionary.ElementAt(new Random().Next(1, dictionary.Count)).Value;
+        }
+    }
 }
